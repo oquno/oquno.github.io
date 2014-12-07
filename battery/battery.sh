8f489c129db29db86de6dd1eb555f4bdf6b7c392
oquno <info@oquno.com>
2014-12-08 00:34:28 +0900

    powercfg のパラメータ修正

--- battery/battery.sh
+++ battery/battery.sh
@@ -2,5 +2,5 @@ USER_NAME=oquno
 OUT_NAME=battery-report.html
 REPO=~/${USER_NAME}.github.io/
 BATTERY_PATH=battery/index.html
-powercfg /batteryreport /output ${REPO}/${BATTERY_PATH}
+powercfg /batteryreport
 mv ${OUT_NAME} ${REPO}${BATTERY_PATH}
