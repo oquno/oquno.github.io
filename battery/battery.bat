65b6a3cc175455236b6286b0fa8b3073b164d287
oquno <info@oquno.com>
2014-12-08 23:17:50 +0900

    バッチスクリプト改良

--- battery/battery.bat
+++ battery/battery.bat
@@ -1,5 +1,5 @@
 @echo off
-set USER_NAME="oquno"
-set REPO_DIR="C:\cygwin\home\%USER_NAME%\%USER_NAME%.github.io"
-powercfg /batteryreport
-move /Y "C:\Users\%USER_NAME%\battery-report.html" "%REPO_DIR%\battery\index.html"
+set USER_NAME=oquno
+set OUT_PATH="C:\cygwin\home\%USER_NAME%\%USER_NAME%.github.io\battery\index.html"
+echo %OUT_PATH%
+powercfg /batteryreport /output %OUT_PATH%
